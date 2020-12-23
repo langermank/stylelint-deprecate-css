298afabee39d26306ba1186f8f3e7b8e33449e89
Katie Langerman <langermank@gmail.com>
2020-12-23 15:16:45 -0500

    well, it does a thing

--- index.js
+++ index.js
@@ -1,62 +1,4 @@
-// @ts-nocheck
-
-'use strict';
-
-
-// Abbreviated example
-// const stylelint = require('stylelint');
-// const isStandardSyntaxRule = require("stylelint/lib/utils/isStandardSyntaxRule");
-// const isStandardSyntaxSelector = require("stylelint/lib/utils/isStandardSyntaxSelector");
-// const parseSelector = require("stylelint/lib/utils/parseSelector");
-// const matchesStringOrRegExp = require("stylelint/lib/utils/matchesStringOrRegExp");
-
-// const ruleName = "plugin/stylelint-deprecate-css";
-// const messages = stylelint.utils.ruleMessages(ruleName, {
-//     unexpected: (tagName) => `Unexpected tag ${tagName} without class qualifier`
-// });
-
-// module.exports = stylelint.createPlugin(ruleName, function (
-//     primaryOption,
-//     secondaryOptionObject
-// ) {
-//     return function (postcssRoot, postcssResult) {
-//         const validOptions = stylelint.utils.validateOptions(
-//             postcssResult,
-//             ruleName,
-//             {
-//                 /* .. */
-//             }
-//         );
-
-//         if (!validOptions) {
-//             return;
-//         }
-
-//         // ... some logic ...
-//         stylelint.utils.report({
-//             /* .. */
-//         });
-//     };
-// });
-
-// module.exports.ruleName = ruleName;
-// module.exports.messages = messages;
-
-// const _ = require('lodash');
 const stylelint = require('stylelint');
-// const isStandardSyntaxRule = require("stylelint/lib/utils/isStandardSyntaxRule");
-// const isStandardSyntaxSelector = require("stylelint/lib/utils/isStandardSyntaxSelector");
-// const parseSelector = require("stylelint/lib/utils/parseSelector");
-// const matchesStringOrRegExp = require("stylelint/lib/utils/matchesStringOrRegExp");
-
-
-// const _ = require('lodash');
-// const isStandardSyntaxAtRule = require('stylelint/lib/utils/isStandardSyntaxAtRule');
-// const report = require('stylelint/lib/utils/report');
-// const ruleMessages = require('stylelint/lib/utils/ruleMessages');
-// const validateOptions = require('stylelint/lib/utils/validateOptions');
-// const vendor = require('stylelint/lib/utils/vendor');
-
 const _ = require('lodash');
 const isCustomProperty = require('stylelint/lib/utils/isCustomProperty');
 const isStandardSyntaxProperty = require('stylelint/lib/utils/isStandardSyntaxProperty');
@@ -68,140 +10,69 @@ const validateOptions = require('stylelint/lib/utils/validateOptions');
 const vendor = require('stylelint/lib/utils/vendor');
 
 const ruleName = 'plugin/stylelint-deprecate-css';
-// const messages = stylelint.utils.ruleMessages(ruleName, {
-//     // unexpected: (tagName) => `Unexpected tag ${tagName} without class qualifier`
-//     rejected: (property) => `Unexpected property "${property}"`,
-// });
-
 const messages = ruleMessages(ruleName, {
-    rejected: (property) => `Unexpected property "${property}"`,
+    unexpected: (property) => `Deprecated classname "${property}"`,
 });
 
-function rule(list) {
+module.exports = stylelint.createPlugin(ruleName, list => {
     return (root, result) => {
-        const validOptions = validateOptions(result, ruleName, {
+
+        // list = array of classnames
+        const validOptions = stylelint.utils.validateOptions(result, ruleName, {
             actual: list,
             possible: [_.isString, _.isRegExp],
-        });
-
-        // Get full selector
-        // const selector = _.get(rule, 'raws.selector.raw', rule.selector);
+        })
 
         if (!validOptions) {
-            return;
+            return
         }
 
-        root.walkDecls((decl) => {
-            // const prop = decl.prop;
-            const selector = _.get(rule, 'raws.selector.raw', rule.selector);
+        root.walkRules(rule => {
+            // find any selector and return list of matches
+            // compare list of matches to list
+            // return matches to list
+            // return error message
 
-            // if (!isStandardSyntaxProperty(prop)) {
-            //     return;
-            // }
 
-            // if (isCustomProperty(prop)) {
-            //     return;
-            // }
-
-            // if (isLessVariable(prop)) {
-            //     // console.log('hey')
-            // }
-
-            // if (!matchesStringOrRegExp(vendor.unprefixed(selector), list)) {
-            //     return;
-            // }
-
+            // trying to write a regex to find less mixins
+            // could do the same for variables @rule
+            // const matchMixin = selector => selector.match(/\.[\w\-] +\(.*\)/)
             // Non-outputting Less mixin definition (e.g. .mixin() {})
-            if (selector.endsWith(')') && !selector.includes(':')) {
-                return;
+            // if (match.endsWith(')') && match.includes(':')) {
+            // if (match.endsWith(')')) {
+            //     // return;
+            //     console.log(match);
+            // }
+
+            // find any css class started with . (I think)
+            const match = selector => selector.match(/\.[-\w]+/g)
+
+            // find all css classes in files
+            const matches = match(rule.selector)
+
+            // compare list to css classes in files and find matches
+            const intersection = list.filter(element => matches.includes(element));
+            console.log(matches)
+
+            // if a match exists throw error
+            // this is probably kinda wrong
+            for (const item of Array.from(intersection)) {
+                if (intersection) {
+                    stylelint.utils.report({
+                        message: messages.unexpected(intersection),
+                        node: rule,
+                        result,
+                        ruleName
+                    })
+                }
             }
 
-            report({
-                message: messages.rejected(selector),
-                node: decl,
-                result,
-                ruleName,
-            });
         });
     };
-}
+});
 
-rule.primaryOptionArray = true;
+// allow consumer to pass items as array in .stylelintrc
+module.exports.rule.primaryOptionArray = true
 
-rule.ruleName = ruleName;
-rule.messages = messages;
-// module.exports = rule;
-module.exports = stylelint.createPlugin(ruleName, rule);
-
-// const rule = function (primaryOption) {
-//     return function (root, result) {
-//         let validOptions = stylelint.utils.validateOptions(result, ruleName, {
-//             actual: primaryOption,
-//             possible: [_.isString]
-//         });
-//         if (!validOptions) {
-//             return;
-//         }
-
-//         function checkSelector(selectorNode, ruleNode) {
-//             let combinedSegments = selectorNode.split(node => {
-//                 return node.type === 'combinator';
-//             });
-
-//             combinedSegments.forEach(segment => {
-//                 let unqualifiedTagNode;
-//                 segment.forEach(node => {
-//                     if (node.type === 'class' && matchesStringOrRegExp(node.value, primaryOption)) {
-//                         unqualifiedTagNode = node;
-//                     }
-//                     if (node.type === 'other') {
-//                         unqualifiedTagNode = void 0;
-//                     }
-//                 });
-
-//                 if (unqualifiedTagNode) {
-//                     stylelint.utils.report({
-//                         ruleName: ruleName,
-//                         result: result,
-//                         node: ruleNode,
-//                         message: messages.unexpected(unqualifiedTagNode.value),
-//                         word: unqualifiedTagNode
-//                     });
-//                 }
-//             });
-//         }
-
-//         function checkSelectorRoot(selectorRootNode, ruleNode) {
-//             selectorRootNode.each(selectorNode => {
-//                 checkSelector(selectorNode, ruleNode);
-//             });
-//         }
-
-//         root.walkRules(ruleNode => {
-//             if (!isStandardSyntaxRule(ruleNode)) {
-//                 return;
-//             }
-//             if (!isStandardSyntaxSelector(ruleNode.selector)) {
-//                 return;
-//             }
-//             if (
-//                 ruleNode.nodes.some(
-//                     node => ["rule", "atrule"].indexOf(node.type) !== -1
-//                 )
-//             ) {
-//                 // Skip unresolved nested selectors
-//                 return;
-//             }
-
-//             ruleNode.selectors.forEach(selector => {
-//                 parseSelector(selector, result, ruleNode, container =>
-//                     checkSelectorRoot(container, ruleNode)
-//                 );
-//             });
-//         });
-//     };
-// };
-// rule.primaryOptionArray = true;
-// rule.ruleName = ruleName;
-// rule.messages = messages;
-// module.exports = stylelint.createPlugin(ruleName, rule);
+module.exports.ruleName = ruleName
+module.exports.messages = messages
